4df6de2d7da3f75cb22aceecb5f87e25c8587005
sebastian <sebastian.danielsson@protonmail.com>
2022-12-21 12:12:13 +0100

    Troubleshooting, testing tip from GitHub
    https://github.com/tailwindlabs/tailwindcss/discussions/7621#discussioncomment-2509792

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import { join } from "path";
 export const darkMode = "class";
-export const content = ["./src/**/*.{html,js}"];
+export const content = [join(__dirname, "src/**/*.{html,js,ts,jsx,tsx}")];
 export const theme = {
     extend: {},
 };
