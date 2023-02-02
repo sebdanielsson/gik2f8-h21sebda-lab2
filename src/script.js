321e75baffb8265eb7f06e50b8ccd02d95638b4b
sebastian <sebastian.danielsson@protonmail.com>
2023-02-02 11:17:31 +0100

    Update to relative API path

--- src/script.js
+++ src/script.js
@@ -5,13 +5,13 @@ const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 
 // Follow user's system theme preference if no website theme preference is set
 prefersDarkMode.addEventListener("change", (event) => {
-    if (!localStorage.getItem("theme")) {
-        if (event.matches) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
+  if (!localStorage.getItem("theme")) {
+    if (event.matches) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
+  }
 });
 
 // Theme switcher
@@ -20,30 +20,30 @@ const themeLight = document.getElementById("theme-light");
 const themeDark = document.getElementById("theme-dark");
 
 themeSystem.addEventListener("click", () => {
-    if (prefersDarkMode.matches) {
-        document.documentElement.classList.add("dark");
-    } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.removeItem("theme");
-    }
+  if (prefersDarkMode.matches) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.removeItem("theme");
+  }
 });
 
 themeLight.addEventListener("click", () => {
-    document.documentElement.classList.remove("dark");
-    localStorage.setItem("theme", "light");
+  document.documentElement.classList.remove("dark");
+  localStorage.setItem("theme", "light");
 });
 
 themeDark.addEventListener("click", () => {
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme", "dark");
 });
 
 // Create new API instance
-const api = new Api("https://gik2f8-lab2-todo.herokuapp.com/tasks");
+const api = new Api("/tasks");
 
 // Load tasks on page load
 window.addEventListener("load", () => {
-    renderTasks();
+  renderTasks();
 });
 
 // Validate fields
@@ -52,77 +52,77 @@ taskForm.titleField.addEventListener("blur", (e) => validateForm(e.target));
 
 let titleValid = false;
 function validateForm(field) {
-    if (field.value.length > 0) {
-        if (titleWarning) {
-            titleWarning.style.display = "none";
-        }
-        titleValid = true;
-    } else {
-        if (titleWarning) {
-            titleWarning.style.display = "block";
-        }
-        titleValid = false;
+  if (field.value.length > 0) {
+    if (titleWarning) {
+      titleWarning.style.display = "none";
     }
+    titleValid = true;
+  } else {
+    if (titleWarning) {
+      titleWarning.style.display = "block";
+    }
+    titleValid = false;
+  }
 }
 
 // Submit form
 function addTaskButton() {
-    if (titleValid) {
-        const newTask = {
-            title: taskForm.titleField.value,
-            description: taskForm.descriptionField.value,
-            dueDate: taskForm.dueDateField.value.replace("T", " "),
-            completed: false,
-        };
-        addTask(newTask);
-    } else {
-        console.log("Form is invalid");
-    }
+  if (titleValid) {
+    const newTask = {
+      title: taskForm.titleField.value,
+      description: taskForm.descriptionField.value,
+      dueDate: taskForm.dueDateField.value.replace("T", " "),
+      completed: false,
+    };
+    addTask(newTask);
+  } else {
+    console.log("Form is invalid");
+  }
 }
 
 // Add task
 function addTask(newTask) {
-    api.create(newTask).then((task) => {
-        if (task) {
-            clearForm();
-            renderTasks();
-        }
-    });
+  api.create(newTask).then((task) => {
+    if (task) {
+      clearForm();
+      renderTasks();
+    }
+  });
 }
 
 // Render tasks
 function renderTasks() {
-    taskList.innerHTML = "";
+  taskList.innerHTML = "";
 
-    api.getAll().then((tasks) => {
-        try {
-            if (tasks && tasks.length === 0) {
-                taskList.innerHTML = `<li class="text-center py-3 border-t border-rose-200"><span class="text-2xl">Nothing to do </span><span class="text-4xl">🤷‍♀️</span></li>`;
-            } else if (tasks && tasks.length > 0) {
-                // Sort tasks by due date
-                tasks.sort((a, b) => {
-                    const dateA = new Date(a.dueDate);
-                    const dateB = new Date(b.dueDate);
-                    if (dateA < dateB) {
-                        return -1;
-                    }
-                    if (dateA > dateB) {
-                        return 1;
-                    }
-                    return 0;
-                });
+  api.getAll().then((tasks) => {
+    try {
+      if (tasks && tasks.length === 0) {
+        taskList.innerHTML = `<li class="text-center py-3 border-t border-rose-200"><span class="text-2xl">Nothing to do </span><span class="text-4xl">🤷‍♀️</span></li>`;
+      } else if (tasks && tasks.length > 0) {
+        // Sort tasks by due date
+        tasks.sort((a, b) => {
+          const dateA = new Date(a.dueDate);
+          const dateB = new Date(b.dueDate);
+          if (dateA < dateB) {
+            return -1;
+          }
+          if (dateA > dateB) {
+            return 1;
+          }
+          return 0;
+        });
 
-                // Sort tasks by completed
-                const completedTasks = tasks.filter((task) => task.completed);
-                const incompleteTasks = tasks.filter((task) => !task.completed);
-                incompleteTasks.forEach((task) => {
-                    taskList.insertAdjacentHTML("beforeend", renderTasksHTML(task));
-                });
-                completedTasks.forEach((task) => {
-                    taskList.insertAdjacentHTML("beforeend", renderTasksHTML(task));
-                });
-            } else {
-                taskList.innerHTML = `
+        // Sort tasks by completed
+        const completedTasks = tasks.filter((task) => task.completed);
+        const incompleteTasks = tasks.filter((task) => !task.completed);
+        incompleteTasks.forEach((task) => {
+          taskList.insertAdjacentHTML("beforeend", renderTasksHTML(task));
+        });
+        completedTasks.forEach((task) => {
+          taskList.insertAdjacentHTML("beforeend", renderTasksHTML(task));
+        });
+      } else {
+        taskList.innerHTML = `
                 <li class="flex text-center py-3 border-t border-rose-200">
                 <svg class="h-7 w-7 my-auto mr-2 dark:fill-white" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22.7399 6.32717C24.3781 8.48282 24.2132 11.571 22.2453 13.5389L20.3007 15.4835C20.0078 15.7764 19.533 15.7764 19.2401 15.4835L12.5226 8.76595C12.2297 8.47306 12.2297 7.99818 12.5226 7.70529L14.4671 5.76075C16.4352 3.79268 19.5237 3.62792 21.6793 5.26646L24.7238 2.22166C25.0167 1.92875 25.4916 1.92873 25.7845 2.22161C26.0774 2.51449 26.0774 2.98936 25.7845 3.28227L22.7399 6.32717ZM19.7704 13.8925L21.1846 12.4783C22.7467 10.9162 22.7467 8.3835 21.1846 6.82141C19.6225 5.25931 17.0899 5.25931 15.5278 6.82141L14.1135 8.23562L19.7704 13.8925Z" />
@@ -132,22 +132,22 @@ function renderTasks() {
                 Error: Could not connect to the server.
                 </span>
                 </li>`;
-            }
-        } catch (error) {
-            console.log("error", error);
-        }
-    });
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  });
 }
 
 function renderTasksHTML({ id, title, description, dueDate, completed }) {
-    let html = `
+  let html = `
         <li id="task_${id}" class="taskItem group flex flex-col gap-6 py-3 border-t border-rose-200">
         <div class="flex gap-3 items-start">
         <div class="flex h-5 items-center">
     `;
-    !completed && (html += `<input onchange="updateCompletion(${id}, this)" name="completed" type="checkbox" title="Completed" class="h-4 w-4 rounded border-gray-300 text-rose-600 focus:ring-rose-500" />`);
-    completed && (html += `<input onchange="updateCompletion(${id}, this)" name="completed" type="checkbox" title="Completed" checked class="h-4 w-4 rounded border-gray-300 text-rose-600 focus:ring-rose-500" />`);
-    html += `
+  !completed && (html += `<input onchange="updateCompletion(${id}, this)" name="completed" type="checkbox" title="Completed" class="h-4 w-4 rounded border-gray-300 text-rose-600 focus:ring-rose-500" />`);
+  completed && (html += `<input onchange="updateCompletion(${id}, this)" name="completed" type="checkbox" title="Completed" checked class="h-4 w-4 rounded border-gray-300 text-rose-600 focus:ring-rose-500" />`);
+  html += `
         </div>
         <details open class="w-full text-sm">
         <summary class="group-hover:text-rose-500 dark:group-hover:text-rose-300 font-medium text-gray-700 dark:text-gray-300">${title}<br>${dueDate}</summary>
@@ -158,102 +158,102 @@ function renderTasksHTML({ id, title, description, dueDate, completed }) {
         </div>
         </li>
         `;
-    return html;
+  return html;
 }
 
 // Add event listener to edit button
 function editTaskButton(id) {
-    api.get(id).then((task) => {
-        if (task) {
-            // Show update button and cancel button, hide add button
-            taskForm.updateButton.classList.remove("hidden");
-            taskForm.cancelUpdateButton.classList.remove("hidden");
-            taskForm.addButton.classList.add("hidden");
+  api.get(id).then((task) => {
+    if (task) {
+      // Show update button and cancel button, hide add button
+      taskForm.updateButton.classList.remove("hidden");
+      taskForm.cancelUpdateButton.classList.remove("hidden");
+      taskForm.addButton.classList.add("hidden");
 
-            // Disable all edit buttons
-            const editButtons = document.querySelectorAll(".editButton");
-            editButtons.forEach((button) => {
-                button.disabled = true;
-                button.classList.add("cursor-not-allowed");
-                button.classList.add("bg-gray-400");
-                button.classList.add("hover:bg-gray-400");
-            });
+      // Disable all edit buttons
+      const editButtons = document.querySelectorAll(".editButton");
+      editButtons.forEach((button) => {
+        button.disabled = true;
+        button.classList.add("cursor-not-allowed");
+        button.classList.add("bg-gray-400");
+        button.classList.add("hover:bg-gray-400");
+      });
 
-            // Fill form with task data
-            taskForm.titleField.value = task.title;
-            taskForm.descriptionField.value = task.description;
-            taskForm.dueDateField.value = task.dueDate;
+      // Fill form with task data
+      taskForm.titleField.value = task.title;
+      taskForm.descriptionField.value = task.description;
+      taskForm.dueDateField.value = task.dueDate;
 
-            // Add event listener to update button
-            taskForm.updateButton.onclick = () => {
-                const updatedTask = {
-                    title: taskForm.titleField.value,
-                    description: taskForm.descriptionField.value,
-                    dueDate: taskForm.dueDateField.value.replace("T", " "),
-                };
+      // Add event listener to update button
+      taskForm.updateButton.onclick = () => {
+        const updatedTask = {
+          title: taskForm.titleField.value,
+          description: taskForm.descriptionField.value,
+          dueDate: taskForm.dueDateField.value.replace("T", " "),
+        };
 
-                // Show update button and cancel button, hide add button
-                taskForm.updateButton.classList.add("hidden");
-                taskForm.cancelUpdateButton.classList.add("hidden");
-                taskForm.addButton.classList.remove("hidden");
+        // Show update button and cancel button, hide add button
+        taskForm.updateButton.classList.add("hidden");
+        taskForm.cancelUpdateButton.classList.add("hidden");
+        taskForm.addButton.classList.remove("hidden");
 
-                // Task updated successfully
-                api.update(id, updatedTask).then((task) => {
-                    if (task) {
-                        renderTasks();
-                        clearForm();
-                    }
-                });
-            };
-        }
-    });
+        // Task updated successfully
+        api.update(id, updatedTask).then((task) => {
+          if (task) {
+            renderTasks();
+            clearForm();
+          }
+        });
+      };
+    }
+  });
 }
 
 function cancelUpdate() {
-    clearForm();
-    taskForm.updateButton.classList.add("hidden");
-    taskForm.cancelUpdateButton.classList.add("hidden");
-    taskForm.addButton.classList.remove("hidden");
+  clearForm();
+  taskForm.updateButton.classList.add("hidden");
+  taskForm.cancelUpdateButton.classList.add("hidden");
+  taskForm.addButton.classList.remove("hidden");
 
-    // Enable edit buttons
-    const editButtons = document.querySelectorAll(".editButton");
-    editButtons.forEach((button) => {
-        button.disabled = false;
-        button.classList.remove("cursor-not-allowed");
-        button.classList.remove("bg-gray-400");
-        button.classList.remove("hover:bg-gray-400");
-    });
+  // Enable edit buttons
+  const editButtons = document.querySelectorAll(".editButton");
+  editButtons.forEach((button) => {
+    button.disabled = false;
+    button.classList.remove("cursor-not-allowed");
+    button.classList.remove("bg-gray-400");
+    button.classList.remove("hover:bg-gray-400");
+  });
 }
 
 // Update completion
 function updateCompletion(id, checkbox) {
-    api.get(id).then((task) => {
-        if (task) {
-            const updatedTask = {
-                completed: checkbox.checked,
-            };
-            api.update(id, updatedTask).then((task) => {
-                renderTasks();
-            });
-        }
-    });
+  api.get(id).then((task) => {
+    if (task) {
+      const updatedTask = {
+        completed: checkbox.checked,
+      };
+      api.update(id, updatedTask).then((task) => {
+        renderTasks();
+      });
+    }
+  });
 }
 
 // Delete task
 function deleteTask(id) {
-    api.remove(id).then((response) => {
-        if (response.status === 404) {
-            console.log("Error deleting task.");
-        } else {
-            console.log(response);
-            renderTasks();
-        }
-    });
+  api.remove(id).then((response) => {
+    if (response.status === 404) {
+      console.log("Error deleting task.");
+    } else {
+      console.log(response);
+      renderTasks();
+    }
+  });
 }
 
 // Clear form
 function clearForm() {
-    taskForm.titleField.value = "";
-    taskForm.descriptionField.value = "";
-    taskForm.dueDateField.value = "";
+  taskForm.titleField.value = "";
+  taskForm.descriptionField.value = "";
+  taskForm.dueDateField.value = "";
 }
